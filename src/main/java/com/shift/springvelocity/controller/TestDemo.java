499780f196e62fe0a86e294d0a40c5a8677ce70b
mukul.munishwar@shiftinc.jp <mukul.munishwar@gmail.com>
2018-01-16 10:40:03 +0530

    GHT-1 : test of git hub 2222nd Commit

--- src/main/java/com/shift/springvelocity/controller/TestDemo.java
+++ src/main/java/com/shift/springvelocity/controller/TestDemo.java
@@ -1,7 +1,62 @@
 package com.shift.springvelocity.controller;
 
-public class TestDemo {
-	
-	//this is sample program
+import java.util.ArrayList;
+import java.util.List;
 
+class my {
+	my(){
+		System.out.println("Constructor my ");
+	}
+	void nn1(){
+		System.out.println("Parent Method");
+	}
+	public static void main(String args[]) {
+		System.out.println("Parent Main");
+	}
+}
+
+class he extends my {
+	he(){
+		System.out.println("Constructor he ");
+	}
+	void nn2(){
+		System.out.println("Child Method");
+		ArrayList<Integer> l = new ArrayList<Integer>();
+	}
+	public static void main(String args[]) {
+		System.out.println("Child Main");
+	}
+}
+
+class TestDemo{
+	
+//	static{
+//		System.out.println("Static Block");
+//	}
+	public static void main(String... args){
+		
+		my m= new he();
+		he h = new he();
+		m.nn1();
+		
+		m.main(new String[] {"",""});
+		
+		final byte a=10;
+		final byte b=11;
+		byte aa = a+b;
+		System.out.println("aa "+aa);
+		
+		Math.sqrt(2);
+		
+		byte any = 127;
+		if(any<0.7){
+			System.out.println("YES");
+		}else{
+			System.out.println("No");
+		}
+		
+		List l = new ArrayList();
+		String p = (String) l.get(0);
+		
+	}
 }
--- src/main/java/com/shift/springvelocity/controller/TestDemo2.java
+++ src/main/java/com/shift/springvelocity/controller/TestDemo2.java
@@ -1,6 +1,43 @@
 package com.shift.springvelocity.controller;
 
-public class TestDemo2 {
-	//Another sample program
+import java.util.ArrayList;
 
+class first {
+	first(){
+		System.out.println("Constructor my ");
+	}
+	void nn1(){
+		System.out.println("In Parent Method");
+	}
+	public static void main(String args[]) {
+		System.out.println("AA");
+	}
+}
+
+class second extends first {
+	second(){
+		System.out.println("Constructor he ");
+	}
+	void nn2(){
+		System.out.println("In Child Method");
+		ArrayList<Integer> l = new ArrayList<Integer>();
+	}
+	public static void main(String args[]) {
+		System.out.println("BB");
+	}
+}
+
+class TestDemo2{
+	
+//	static{
+//		System.out.println("Static Block");
+//	}
+	public static void main(String... args) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		
+		Object m= Class.forName("my").newInstance();
+		he h = new he();
+		((my) m).nn1();
+		
+		((my) m).main(new String[] {"",""});
+	}
 }
